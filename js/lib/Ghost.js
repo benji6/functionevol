5353bdb99bcc389f9af08f7731dd1fdba776b1d6
benji6 <benhall22@hotmail.co.uk>
2015-01-20 19:20:32 +0000

    working on chromosomes and dominance

--- js/lib/Ghost.js
+++ js/lib/Ghost.js
@@ -11,18 +11,26 @@ var Ghost = function (arity, length) {
       initialFunctions.fns,
       initialFunctions.fns
   ],
-  this.fns,
-  this.outputs;
+  this.fns = [],
+  this.outputs = [];
 };
 
 Ghost.prototype.computeFns = function () {
-  var chromosomes0Length = this.chromosomes[0].length;
-  var chromosomes1Length = this.chromosomes[1].length;
-  var length = Math.max(chromosomes0Length, chromosomes1Length);
-
-  //working on this
-
-  this.fns = this.chromosomes[0];
+  chromosome0 = this.chromosomes[0];
+  chromosome1 = this.chromosomes[1];
+  var length = Math.max(chromosome0.length, chromosome1.length);
+  var i;
+  var dominance0;
+  var dominance1;
+  for (i = 0; i < length; i++) {
+    dominance0 = chromosome0[i] && chromosome0[i].dominance || 0;
+    dominance1 = chromosome1[i] && chromosome1[i].dominance || 0;
+    if (dominance0 >= dominance1) {
+      this.fns[i] = chromosome0[i];
+    } else {
+      this.fns[i] = chromosome0[i];
+    }
+  }
 };
 
 module.exports = Ghost;
