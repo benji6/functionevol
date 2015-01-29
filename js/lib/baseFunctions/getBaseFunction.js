ad30751d78ed45e78e79e9a450f9047927d93683
benji6 <benhall22@hotmail.co.uk>
2015-01-29 23:21:54 +0000

    make get function a lot more flexible

--- js/lib/baseFunctions/getBaseFunction.js
+++ js/lib/baseFunctions/getBaseFunction.js
@@ -1,10 +1,13 @@
 var baseFunctions = require('./lib/index.js');
 
-module.exports = {
-  dominance: function (lib, index) {
-    return lib.dominances[index];
-  },
-  fn: function (lib, index) {
-    return lib.fns[index];
-  }
-};
+['dominance', 'fn', 'name'].forEach((elem) => {
+  module.exports[elem] = function (lib, index) {
+    return lib[elem + 's'][index];
+  };
+  module.exports[elem + 's'] = function (libs, indices) {
+    return libs.reduce(function (acc, lib, i) {
+      acc.push(lib[elem + 's'][indices[i]]);
+      return acc;
+    }, []);
+  };
+});
