b4bb19cd3f063cfa743af85b285e38ef3833b12e
benji6 <benhall22@hotmail.co.uk>
2015-01-31 11:08:01 +0000

    match spec

--- js/lib/reproduction/sexual.js
+++ js/lib/reproduction/sexual.js
@@ -1,15 +1,14 @@
 var Ghost = require('../Ghost/Ghost.js');
 
-var shouldI = (prob) => Math.random() < prob;
+// var shouldI = (prob) => Math.random() < prob;
+
+var getRandom = (int) => Math.floor(Math.random() * int);
+var meiosis = (chromosomes) => chromosomes[getRandom(chromosomes.length)];
 
 module.exports = function (parent0, parent1) {
-  //parents are randomly selected
   var child = Ghost(parent0.arity, parent0.length);
-  var getRandom = (int) => Math.floor(Math.random() * int);
-  var random0 = getRandom(2);
-  var random1 = getRandom(2);
-  child.chromosomes[0] = parent0.chromosomes[random0];
-  child.chromosomes[1] = parent1.chromosomes[random1];
+  child.chromosomes[0] = meiosis(parent0.chromosomes);
+  child.chromosomes[1] = meiosis(parent1.chromosomes);
   child.chromosomes.sort(function (a, b) {
     return a.libs.length < b.libs.length;
   });
