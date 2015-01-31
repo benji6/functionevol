916178af3a4a988f49f10c616824f53eb8571ac3
benji6 <benhall22@hotmail.co.uk>
2015-01-31 11:03:32 +0000

    update reproduce spec

--- tests/lib/reproduceSpec.js
+++ tests/lib/reproduceSpec.js
@@ -2,35 +2,33 @@ var reproduce = require('../../js/lib/reproduction/sexual.js');
 var Ghost = require('../../js/lib/Ghost/Ghost.js');
 var computeFns = require('../../js/lib/Ghost/computeFns.js');
 
+var GhostSpec = require('./GhostSpec.js');
+
 module.exports = () => {
   describe("reproduce", () => {
-    it("is a function", () =>
-    expect(reproduce).toEqual(jasmine.any(Function)));
-    describe("reproduce(Ghost(5, 6), Ghost(5, 6))", () => {
-      it("returns an object", () =>
-      expect(reproduce(Ghost(5, 6), Ghost(5, 6))).toEqual(jasmine.any(Object)));
-      describe("var child = reproduce(Ghost(1, 9), Ghost(5, 8))", () => {
-        var child = reproduce(Ghost(1, 9), Ghost(5, 8));
-        describe("chromosomes", () => {
-          it("is an array of length 2", () => {
-            expect(child.chromosomes).toEqual(jasmine.any(Array));
-            expect(child.chromosomes.length).toEqual(2);
-          });
+    describe("var child = reproduce(Ghost(5, 8), Ghost(1, 9))", () => {
+      var child;
+      beforeEach(() => child = reproduce(Ghost(5, 8), Ghost(1, 9)));
+      describe("chromosomes propery", () => {
+        it("is an array of 2 objects", () => {
+          expect(child.chromosomes).toEqual(jasmine.any(Array));
+          expect(child.chromosomes.length).toBe(2);
+          expect(child.chromosomes.every((elem) =>
+            elem.constructor === Object)).toBe(true);
         });
-        describe("computeFns(child)", () => {
-          computeFns(child);
-          it("is an object", () =>
-          expect(child).toEqual(jasmine.any(Object)));
-          describe("child.fns", () => {
-            it("is an array", () =>
-            expect(child.fns).toEqual(jasmine.any(Array)));
-            it("of length 9", () =>
-            expect(child.fns.length).toEqual(9));
-            it("all elements are functions", () =>
-            expect(child.fns.every((element) =>
-            typeof element === "function")).toEqual(true));
-            console.log(child);
-          });
+        describe("chromosomes[0] and chromosomes[1]", () => {
+          it("meet the chromosome spec as defined in Ghost \
+            and are ordered by descending libs length", () => {
+              expect(child.chromosomes).toEqual(jasmine.any(Object));
+              expect(child.chromosomes[0].libs).toEqual(jasmine.any(Array));
+              expect(child.chromosomes[0].libs.length).toBe(9);
+              expect(child.chromosomes[0].libs.every((elem) =>
+                elem.constructor === Object));
+              expect(child.chromosomes[1].indices).toEqual(jasmine.any(Array));
+              expect(child.chromosomes[1].indices.length).toBe(8);
+              expect(child.chromosomes[1].indices.every((elem) =>
+                elem.constructor === Number));
+            });
         });
       });
     });
